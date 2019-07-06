c5012874c1987aa58fa813b43c78ceacbcaa28bc
blitheanon <blitheanon@qq.com>
2019-07-06 09:12:51 +0800

    Feature: 通过Props传递数据, this.props.value

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ class Square extends React.Component {
     return (
       <button className="square">
         {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
@@ -16,7 +17,7 @@ class Square extends React.Component {
 //组件Board
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
