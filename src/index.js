30ea2bacdf5540eab3981bab98a8c725208e24cf
blitheanon <blitheanon@qq.com>
2019-07-07 12:37:44 +0800

    Feature: 新增时间旅行功能

--- src/index.js
+++ src/index.js
@@ -43,22 +43,13 @@ class Board extends React.Component {
         };
     }
 
-handleClick(i){
-    const squares=this.state.squares.slice();
-    //游戏结束，不继续
-    if (calculateWinner(squares)||squares[i]) {
-        return;
-    }
-    //反转玩家状态
-    squares[i]=this.state.xIsNext?'X':'O';
-    this.setState({squares: squares,xIsNext: !this.state.xIsNext});
-}
 
   renderSquare(i) {
     return (
     <Square
-    value={this.state.squares[i]}
-    onClick={()=>this.handleClick(i)}
+    // value={this.state.squares[i]}
+    value={this.props.squares[i]}
+    onClick={()=>this.props.onClick(i)}
     />
     );
   }
@@ -66,17 +57,15 @@ handleClick(i){
   render() {
     //下棋提示
     //const status = 'Next player: '+(this.state.xIsNext?'X':'O');
-
-    const winner= calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-        status='Winner: '+winner;
-    }else{
-        status='Next player:'+(this.state.xIsNext?'X':'O');
-    }
+    // const winner= calculateWinner(this.state.squares);
+    // let status;
+    // if (winner) {
+    //     status='Winner: '+winner;
+    // }else{
+    //     status='Next player:'+(this.state.xIsNext?'X':'O');
+    // }
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -99,15 +88,77 @@ handleClick(i){
 
 //组件Game
 class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+            history:[{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i){
+        const history=this.state.history.slice(0,this.state.stepNumber+1);
+        const current=history[history.length-1];
+        const squares=current.squares.slice();
+        //游戏结束，不继续
+        if (calculateWinner(squares)||squares[i]) {
+            return;
+        }
+        //反转玩家状态
+        squares[i]=this.state.xIsNext?'X':'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+//更新stepNumber
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step%2)===0,
+        })
+    }
+
   render() {
+      const history=this.state.history;
+      const current=history[this.state.stepNumber];
+      const winner=calculateWinner(current.squares);
+
+      const moves=history.map((step,move)=>{
+          const desc=move?
+          'Go to move #'+move:
+          'Go to game start';
+          return (
+              <li key={move}>
+                <button onClick={()=>this.jumpTo(move)}>{desc}</button>
+              </li>
+          );
+      });
+
+      let status;
+      if (winner) {
+          status='Winner: '+winner;
+      }else{
+          status='Next player: '+(this.state.xIsNext?'X':'O');
+      }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i)=>this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
